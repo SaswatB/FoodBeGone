94a23b5d90aba67be71d24d2729b62bbee120ae3
Simon <simon980425@126.com>
2020-02-15 14:30:16 -0800

    project init

---
+++ backend/FoodBeGone/src/main/java/com/example/FoodBeGone/FoodBeGoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodBeGone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBeGoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBeGoneApplication.class, args);
+	}
+
+}
