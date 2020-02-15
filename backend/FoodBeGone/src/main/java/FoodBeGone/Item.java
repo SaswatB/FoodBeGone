0afec2267cf4d4574003508d742fc0bd66bf1ee1
erickimme <jaeyeonkim0919@gmail.com>
2020-02-15 15:51:52 -0800

    item, itemTemplate, transaction creation

---
+++ backend/FoodBeGone/src/main/java/FoodBeGone/Item.java
@@ -0,0 +1,23 @@
+package FoodBeGone;
+
+import java.util.Date;
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Item {
+    @Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+    
+    private String id;
+    private int count;    
+    private ItemTemplate item_template;
+    private LocalTime available_til;
+    private int disc_percent;
+}
---
+++ backend/FoodBeGone/src/main/java/FoodBeGone/ItemTemplateRepository.java
@@ -0,0 +1,6 @@
+package FoodBeGone;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemTemplateRepository extends CrudRepository<User, String>{
+}
